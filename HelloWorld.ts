a8aa50531d3a8a895bbe67fb6b3b4f690c50be79
jkocina <Jkocina.jr@gmail.com>
2018-03-14 19:20:34 -0500

    Hello World with a class and constructor

--- HelloWorld.ts
+++ HelloWorld.ts
@@ -1,3 +1,10 @@
+class Student {
+	fullName: string;
+	constructor(public firstName: string, public middleInitial: string, public lastName: string)  {
+		this.fullName = firstName + " " + middleInitial + " " + lastName;
+	}
+}
+
 interface Person {
 
 	firstName: string;
@@ -11,6 +18,6 @@ function greeter(person: Person) {
 	
 }
 
-let user = { firstName: "Jane", lastName: "User"};
+let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
